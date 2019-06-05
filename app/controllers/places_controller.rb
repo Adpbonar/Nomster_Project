cbfb6938c85ff58dbb8d230d48f43ab1a76fcf01
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-05 19:38:11 +0000

    Login style

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,15 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.all.order("name").page(params[:page]).per_page(3)
   end
 
   def new
-    @place = Place.new
+    @places = Place.new
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
