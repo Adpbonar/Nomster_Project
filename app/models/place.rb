5b94e9aad99b69f406ca73d7ccdc2dea24b3bf92
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-06 17:37:58 +0000

    Validations Complete

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true
+  validates :name, length: {minimum: 3}
+  validates :address, length: {minimum: 5}
+  validates :description, length: {minimum: 10}
 end
