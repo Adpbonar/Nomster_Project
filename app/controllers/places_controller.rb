b56bd38803b4d6a2596ac139a5689a479238f0ae
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-06-18 00:38:14 +0000

    Index test

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :eidt, :update, :destroy]
   def index
     @places = Place.all.order("name").page(params[:page]).per_page(4)
-    @photo = Photo.find(19)
+    @photo = Photo.find(1)
   end
 
   def new
